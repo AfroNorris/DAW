6d6de128326fbcea0506f09ac2d8646da0b20c8b
tiberiu <tiberiu@192.168.1.40>
2019-03-07 10:09:53 +0100

    3 int random entre 1 y 10

---
+++ EjerciciosJava/src/ejerciciosjava/PruebasGitHub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author tiberiu
+ */
+public class PruebasGitHub {
+    public static void main(String[] args){
+        int a,b,c;
+     
+            a=(int)((Math.random()*10)+1);
+            b=(int)((Math.random()*10)+1);
+            c=(int)((Math.random()*10)+1);
+            System.out.println("a vale"+a);
+            System.out.println("b vale"+b);
+            System.out.println("c vale"+c);
+            
+        
+    }
+}
